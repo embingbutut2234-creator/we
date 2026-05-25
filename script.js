d0419114c102cc16e5aaa0bf304e960a1d811a52
embingbutut2234-creator <embingbutut2234@gmail.com>
2026-05-25 22:19:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+menuToggle.addEventListener("click", () => {
+
+  navMenu.classList.toggle("active");
+
+  menuToggle.classList.toggle("active");
+
+});
